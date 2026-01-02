96859db3ae5cc3a9aa70627b642b4a0aa84cb74f
NotAdi08 <foradityas26@gmail.com>
2026-01-01 23:00:49 -0800

    az sig

--- include/autos.hpp
+++ include/autos.hpp
@@ -1,5 +1,7 @@
 extern void soloPleaseHit();
 extern void Split7Left();
+extern void Split9Left();
 extern void right9();
 extern void pidTuning();
+extern void driveForward();
 extern void skills();
